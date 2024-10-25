56015b0550abde2e99143fad4aec01b3bf9b0707
bharath <bh814281@gmail.com>
2024-10-25 11:40:13 +0530

    small change in padding nothing much

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-600 text-white p-4 mt-4">
+    <footer className="bg-blue-600 text-white p-3 mt-4">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
       </div>
